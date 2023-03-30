feb55fee1b4ee10a8e2a47a671070dd0302ac3e1
ginofft <ntnghia.andrei@gmail.com>
2023-03-30 17:19:55 +0700

    hahahahahahah

--- src/backend/src/dataset.py
+++ src/backend/src/dataset.py
@@ -1,11 +1,9 @@
 import torch
 from types import SimpleNamespace
 from pathlib import Path
+import cv2
 
-from .utils import *
-from .Descriptors import compute_SIFT
-
-class ImageDataset(torch.utils.data.Dataset):
+class ImageDataset1(torch.utils.data.Dataset):
   """This class handle data loading - Pytorch style
   """
   default_conf = {
@@ -41,3 +39,32 @@ class ImageDataset(torch.utils.data.Dataset):
     return data
   def __len__(self):
     return len(self.names)
+  
+class ImageDataset(torch.utils.data.Dataset):
+  default_conf = {
+    'globs': ['*.jpg', '*.png', '*.jpeg', '*.JPG', '*.PNG'],
+    'grayscale': False,
+    'interpolation': 'cv2_area'
+  }
+  def __init__(self, root):
+    self.root = root
+    self.names = []
+    for g in self.default_conf['globs']:
+      self.names += list(Path(root).glob('**/'+g))
+    if len(self.names) == 0:
+      raise ValueError(f'Could not find any image in root: {root}.')
+    self.names = [i.relative_to(self.root).as_posfix() for i in self.names]
+  
+  def __getitem__(self, idx):
+    image = _read_image(self.root/self.names[idx])
+    return image
+
+  def __len__(self):
+    return len(self.names)
+
+  def _read_image(self, path):
+    image = cv2.imread(str(path))
+    if self.default_conf['grayscale']:
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
+
