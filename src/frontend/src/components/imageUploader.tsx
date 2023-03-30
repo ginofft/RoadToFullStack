eb608685691c4cfcf35eb24c9e0a5155dbb7920d
ginofft <ntnghia.andrei@gmail.com>
2023-03-30 15:07:52 +0700

    i fucking did it lol

--- src/frontend/src/components/imageUploader.tsx
+++ src/frontend/src/components/imageUploader.tsx
@@ -1,50 +1,56 @@
-import React, { useState } from "react"
-import './imageUploader.css'
+import React, { useState } from "react";
+import "./imageUploader.css";
 
-function imageUploader(){
-    const [selectedImage, setSelectedImage] = useState<File | null>(null)
-    const [uploadedImages, setUploadedImages] = useState([])
+function imageUploader() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
 
-    function hanldeImageUpload(event){
-        const file = event.target.files[0]
-        const reader = new FileReader()
-        reader.readAsDataURL(file)
-        reader.onloadend = () => {
-            setUploadedImages([...uploadedImages, reader.result])
-        }
+  function handleImageUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setUploadedImages([...uploadedImages, reader.result as string]);
+      };
     }
+  }
 
-    function handleImageClick(image){
-        setSelectedImage(image)
-    }
+  function handleImageClick(image: string) {
+    setSelectedImage(image);
+  }
 
-    return (
-        <div className="image-uploader">
-            <div className='selected-image'>
-                {selectedImage ? (
-                    <img src={selectedImage} alt="selected" height="300px"/>
-                ) : (   
-                    <p>No Image Selected</p>
-                )
-            }
-            </div>
-            <div className="image-list">
-                <ul>
-                    {uploadedImages.map((image) => (    
-                        <li key={image} onClick={() => handleImageClick(image)}>
-                            <img src={image} alt="uploaded" height="50px"/>
-                        </li>
-                    ))}
-                </ul>
-                <input
+  return (
+    <div className="image-uploader">
+      <div className="selected-image">
+        {selectedImage ? (
+          <img src={selectedImage} alt="selected" />
+        ) : (
+          <p>No Image Selected</p>
+        )}
+      </div>
+      <div className="image-list-wrapper">
+        <div className="image-list">
+            <ul>
+            {uploadedImages.map((image) => (
+                <li key={image} onClick={() => handleImageClick(image)}>
+                <img src={image} alt="uploaded" />
+                </li>
+            ))}
+            </ul>
+        </div>
+        <div className="upload-btn">
+            <label htmlFor="file-input">Choose File</label>
+            <input
+                id="file-input"
                 type="file"
                 accept="image/*"
-                onChange={hanldeImageUpload}
-                className="upload-btn"
-                />
-            </div>
+                onChange={handleImageUpload}
+            />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default imageUploader;
